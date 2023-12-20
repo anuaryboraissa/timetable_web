bf7a420d22d29f1ebed60a4dc1d55976fbcfe006
anuaryboraissa <102548370+anuaryboraissa@users.noreply.github.com>
2023-12-20 10:02:28 +0300

    Update app.js
    testing

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app=express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
-app.use("/app/api",require("./service/route"))
+app.get('/'.(req,res)=>res.json({message:"hellow timetable"}))
+// app.use("/app/api",require("./service/route"))
 var port=process.env.port || 5000
 app.listen(port,function(){
   console.log("sever running in port: ",port);
